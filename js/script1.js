5f29ae4ad37048611c8cbeba1bb77a331967306a
Adrián Martinez <adrianesteban42@gmail.com>
2024-06-07 21:30:34 +0200

    lista de personajes

--- js/script1.js
+++ js/script1.js
@@ -7,6 +7,10 @@ async function fetchData(url) {
   return data
 }
 
+$(document).ready(function(){
+  $('#myTable').dataTable();
+});
+
 fetchData("https://thronesapi.com/api/v2/Characters")
 .then(json => {
 
@@ -18,49 +22,33 @@ fetchData("https://thronesapi.com/api/v2/Characters")
         a.setAttribute("href", `detalles.html?id=${json[i].id}`);
         a.setAttribute("class", "character-link");
 
-        const div1 = document.createElement("div")
+        const tbody = document.createElement("tbody")
 
-        div1.setAttribute("class", "div1 col-4")
+        const tr = document.createElement("tr")
 
-        const div2 = document.createElement("div")
+        const td1 = document.createElement("td")
 
-        div2.setAttribute("class", "card")
-        div2.setAttribute("style", "width: 18rem;")
+        td1.innerHTML = json[i].fullName;
+
+        const td2 = document.createElement("td")
 
         const image = document.createElement("img")
 
         image.setAttribute("src" , json[i].imageUrl)
         image.setAttribute("class", "card-img-top")
-        image.setAttribute("alt", "...")
+        image.setAttribute("alt", "...") 
 
-        const div3 = document.createElement("div")
+        tr.appendChild(td1)
+        a.appendChild(image)
+        td2.appendChild(a)
+        tr.appendChild(td2)
+        tbody.appendChild(tr)
 
-        div3.setAttribute("class", "card-body")
+        const contenedor = document.getElementsByClassName("table")[0]
+        contenedor.appendChild(tbody)
 
-        const h5 = document.createElement("h5")
-
-        h5.setAttribute("class", "card-title")
-        h5.innerHTML = json[i].fullName;
-
-        div3.appendChild(h5)
-        div2.appendChild(image)
-        div2.appendChild(div3)
-        a.appendChild(div2)
-        div1.appendChild(a)
-
-        const contenedor = document.getElementsByClassName("row")[0]
-        contenedor.appendChild(div1)
+        
     }
 })
 .catch(error => console.log(error))
 
-/* 
-      <div class="col-4">
-        <div class="card" style="width: 18rem;">
-          <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQUlPwsl7IZOzDF0DrYTyJt21cXUjDzm4X7ZVpeHhzUcsj2ioq-" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Brienne de Tarth</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
- */
